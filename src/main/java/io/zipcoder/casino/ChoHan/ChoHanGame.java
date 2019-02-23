df7153ca199821f764a1287d1733851d2619638c
Nirmala <nirmala.talari@gmail.com>
2019-02-23 17:13:42 -0500

    updated ChoHan game

--- src/main/java/io/zipcoder/casino/ChoHan/ChoHanGame.java
+++ src/main/java/io/zipcoder/casino/ChoHan/ChoHanGame.java
@@ -1,12 +1,69 @@
 package io.zipcoder.casino.ChoHan;
 
-public class ChoHanGame {
-    public java.util.ArrayList<io.zipcoder.casino.utilities.Player> player;
+import io.zipcoder.casino.Interfaces.GamblingGame;
+import io.zipcoder.casino.Interfaces.GamblingPlayer;
+import io.zipcoder.casino.Interfaces.Game;
+import io.zipcoder.casino.Player;
+import io.zipcoder.casino.utilities.Dice;
 
-    public Integer playGame() {
+import java.util.ArrayList;
+
+public class ChoHanGame implements Game, GamblingGame {
+
+    private ArrayList<Player> playerListCHGame = new ArrayList<Player>();
+    private ChoHanPlayer choHanPlayerObj;
+
+    //
+    public ChoHanGame(ArrayList<Player> playerList) {
+        this.playerListCHGame = playerList;
+        choHanPlayerObj = new ChoHanPlayer(playerListCHGame);
+    }
+
+    // default constructor
+    public ChoHanGame() {}
+
+    public ArrayList<Player> getPlayerListChoHan() {
+        return playerListCHGame;
+    }
+
+    public void setPlayerListChoHan(ArrayList<Player> playerListChoHan) {
+        this.playerListCHGame = playerListChoHan;
+    }
+
+    public void start() {
+        getPlayerListChoHan();
+
+
+    }
+
+    public Integer askForBet(GamblingPlayer gamblingPlayer) {
         return null;
     }
 
-    public void getDiceResult() {
+    public Integer getDiceResult() {
+        Integer throwSum = Dice.throwAndSum(2);
+        return throwSum;
     }
+
+    public void loadPlayer() {
+
+    }
+
+    public String getNextPlayerAction() {
+        return null;
+    }
+
+    public void displayGameState() {
+
+    }
+
+    public Boolean playAgain() {
+        return null;
+    }
+
+    public void exit() {
+        System.exit(0);
+
+    }
+
 }
--- src/main/java/io/zipcoder/casino/ChoHan/ChoHanPlayer.java
+++ src/main/java/io/zipcoder/casino/ChoHan/ChoHanPlayer.java
@@ -1,19 +1,62 @@
 package io.zipcoder.casino.ChoHan;
 
-public class ChoHanPlayer {
-    private java.util.ArrayList playerList;
-    private Integer playerBalance;
-    private Integer playerGuess;
+import io.zipcoder.casino.Player;
 
-    public Integer updatePlayerWallet() {
-        return null;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class ChoHanPlayer {
+    private ArrayList<Player> playerList = new ArrayList<Player>();
+    private ArrayList<ChoHanPlayer> eligiblePlayerListCH = new ArrayList<ChoHanPlayer>();
+//    private Integer walletBalanceChoHanPlayer;
+//    private Integer netGainLossChoHanPlayer;
+    private Integer guessChoHanPlayer;
+
+    private String playerNameCH;
+    private Integer walletBalanceCH;
+    public Integer NetGainLossPlayerCH;
+
+    public ChoHanPlayer(ArrayList<Player> playerList) {
+        this.playerList = playerList;
     }
+
+    public ChoHanPlayer(String name, Integer walletBalance, Integer netGainLoss) {
+        this.playerNameCH = name;
+        this.walletBalanceCH = walletBalance;
+        this.NetGainLossPlayerCH = netGainLoss;
+    }
+
+    /*construct the ChoHan eligible playerlist with players that have
+     enough wallet balance to play the game */
+    public ArrayList playerListChoHan(ArrayList playerList) {
+        ListIterator<String> listIterator = playerList.listIterator();
+        Player playerObj = new Player();
+        while (listIterator.hasNext()) {
+            if (playerObj.getWallet() > 0) {
+                eligiblePlayerListCH.add(new ChoHanPlayer(playerObj.getName(), playerObj.getWallet(), playerObj.getPlayerNetGainLoss()));
+            }
+        }
+        return eligiblePlayerListCH;
+    }
+
+    public Integer getWalletBalanceChoHanPlayer() {
+        return walletBalanceCH;
+    }
+
+    public void setWalletBalanceChoHanPlayer(Integer playerBalance) {
+        this.walletBalanceCH = playerBalance;
+    }
+
+//  public Integer updatePlayerWallet() {
+//        return null;
+//    }
 
     public Integer getHouseBalance() {
         return null;
     }
 
     public void getPlayerGuess() {
+
     }
 
     public void getPlayerBets() {
