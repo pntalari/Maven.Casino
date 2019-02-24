48f19ff86c51fc09bcae6252424945521e704bb7
Nirmala <nirmala.talari@gmail.com>
2019-02-24 11:54:53 -0500

    PlayerGuess

--- src/main/java/io/zipcoder/casino/ChoHan/ChoHanGame.java
+++ src/main/java/io/zipcoder/casino/ChoHan/ChoHanGame.java
@@ -3,20 +3,31 @@ package io.zipcoder.casino.ChoHan;
 import io.zipcoder.casino.Interfaces.GamblingGame;
 import io.zipcoder.casino.Interfaces.GamblingPlayer;
 import io.zipcoder.casino.Interfaces.Game;
+import io.zipcoder.casino.utilities.Console;
 import io.zipcoder.casino.utilities.Dice;
 import io.zipcoder.casino.utilities.Player;
-
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ChoHanGame implements Game, GamblingGame {
 
-    private ArrayList<Player> playerListCHGame = new ArrayList<Player>();
+    private ArrayList<Player> playerList = new ArrayList<Player>();
     private ChoHanPlayer choHanPlayerObj;
+    private Integer numberOfDices = 2;
+    private Integer playerBet = 100;
+    private String playerGuessIn = "";
+    private Integer playerGuess;
+
+    private Scanner input;
+    private PrintStream output;
+    //private Console inOut = new Console(InputStream, PrintStream);
 
     // constructor with player list
     public ChoHanGame(ArrayList<Player> playerList) {
-        this.playerListCHGame = playerList;
-        choHanPlayerObj = new ChoHanPlayer(playerListCHGame);
+        this.playerList = playerList;
+        choHanPlayerObj = new ChoHanPlayer(playerList);
     }
 
     // default constructor
@@ -26,22 +37,55 @@ public class ChoHanGame implements Game, GamblingGame {
 //        return playerListCHGame;
 //    }
 
-    public void setPlayerListChoHan(ArrayList<Player> playerListChoHan) {
-        this.playerListCHGame = playerListChoHan;
+    public void setPlayerListChoHan(ArrayList<Player> playerList) {
+
+        choHanPlayerObj.playerListChoHan(playerList);
     }
 
     public void start() {
-       // choHanPlayerObj.getEligiblePlayerListCH();
 
+        //creat the initial ChoHan eligible playerlist with enough wallet balance
+        setPlayerListChoHan(playerList);
+        // how to use the Console
+
+        /** get the player guesses input */
+        playerGuessIn = input.nextLine();
+        playerGuess = getPlayerGuess(playerGuessIn);
+
+        /** get the player bets input */
+        // get the number of dices to roll
+        // get the house balance
+        // get the dice roll throw and sum
+        // randomly pick the players win/losses
+        // update the players wallets
+        // update the house wallet
 
     }
 
+    /** check the player guess is for even or odd
+     * @return integer guess*/
+    public Integer getPlayerGuess(String playerGuessIn){
+        if (playerGuessIn.equalsIgnoreCase("even"))
+        {
+            this.playerGuess = 2;
+        }
+        else if(playerGuessIn.equalsIgnoreCase("odd"))
+        {
+            this.playerGuess = 1;
+        }
+
+        return playerGuess;
+    }
+
+
     public Integer askForBet(GamblingPlayer gamblingPlayer) {
+
         return null;
     }
 
-    public Integer getDiceResult() {
-        Dice dice = new Dice(2);
+
+    public Integer getDiceResult(Integer numberOfDices) {
+        Dice dice = new Dice(numberOfDices);
         return 0;
     }
 
