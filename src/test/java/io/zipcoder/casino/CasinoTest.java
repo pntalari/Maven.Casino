42c10f24f929e52bb889cb0193106cd30bb18d14
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-02-25 17:48:58 -0500

    gitignore changed

--- src/test/java/io/zipcoder/casino/CasinoTest.java
+++ src/test/java/io/zipcoder/casino/CasinoTest.java
@@ -1,5 +1,33 @@
 package io.zipcoder.casino;
 
 
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Console;
+import java.io.PrintStream;
+
 public class CasinoTest {
+
+//
+//    @Test
+//    public void printBannerTest(){
+//        // Given
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        Console console = getConsoleWithBufferedInputAndOutput("600\n200",baos);
+//
+//    }
+//
+////    public Console getConsoleWithBufferedInputAndOutput(String input, ByteArrayOutputStream baos) {
+////        Console testConsole;
+////        if (input == null) {
+////            testConsole = new Console(System.in, new PrintStream(baos));
+////        } else {
+////            ByteArrayInputStream bais = new ByteArrayInputStream(input.getBytes());
+////            testConsole = new Console(bais, new PrintStream(baos));
+////        }
+////
+////        return testConsole;
+////    }
 }
